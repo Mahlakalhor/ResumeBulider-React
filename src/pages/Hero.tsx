3606b9309a9ea6b8d208e21392139c019994e3af
Mahla <Mahlakalhor82@gmail.com>
2025-11-19 14:04:01 +0330

    v1

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,13 +1,15 @@
 import { Feature } from "../components/Feature";
 import { Header } from "../components/Header";
 import { Home } from "../components/Home";
+import TestimonialCard from "../components/TestimonialCard";
 
 export const Hero = () => {
   return (
     <div className="flex flex-col gap-48 ">
       <Header />
       <Home />
-      <Feature/>
+      <Feature />
+      <TestimonialCard/>
     </div>
   );
 };
